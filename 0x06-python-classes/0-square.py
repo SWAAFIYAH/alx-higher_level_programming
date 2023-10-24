3f7b045bf8b1a3e78bc9d073377f11c471b5d079
SWAAFIYAH <swaafiyah9@gmail.com>
2023-10-24 11:05:45 -0700

    python classes and objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for a square"""
+class square:
+    pass
+vim 2-square
+#!/usr/bin/python3
+"""a square module"""
+class square:
+    def __init__(self, length):
+        self .__length = length
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+My python classes and objects class
